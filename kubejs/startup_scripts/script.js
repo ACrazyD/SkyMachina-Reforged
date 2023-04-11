aee2093e512be11608e8f7ad810a319f068310e5
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2023-04-10 22:11:40 -0700

    Starting Out

---
+++ kubejs/startup_scripts/script.js
@@ -0,0 +1,39 @@
+// priority: 0
+
+console.info('Dont mind me, just createing some new things...')
+
+StartupEvents.registry('item', event => {
+
+	//Tools - RockSmashers
+	event.create('skymachina:prospector_spade', 'shovel').tier('stone')
+	event.create('skymachina:stone_rock_smasher', 'pickaxe').tier('stone')
+	event.create('skymachina:iron_rock_smasher', 'pickaxe').tier('iron')
+	event.create('skymachina:diamond_rock_smasher', 'pickaxe').tier('diamond')
+	event.create('skymachina:netherite_rock_smasher', 'pickaxe').tier('netherite')
+
+
+	//Dummy Ores
+	event.create('skymachina:cobblestone_pebble')
+	event.create('skymachina:iron_chunk')
+})
+
+StartupEvents.registry('block', event => {
+	
+})
+
+StartupEvents.registry('fluid', event => {
+	
+    event.create('skymachina:liquid_glass').thinTexture(0xFFF2E2).bucketColor(0xFFF2E2).displayName('Liquid Glass')
+})
+
+
+ItemEvents.modification(event => {
+
+	event.modify('minecraft:oak_leaves', item => {item.burnTime = 100})
+	event.modify('minecraft:birch_leaves', item => {item.burnTime = 100})
+	event.modify('minecraft:jungle_leaves', item => {item.burnTime = 100})
+	event.modify('minecraft:dark_oak_leaves', item => {item.burnTime = 100})
+	event.modify('minecraft:acacia_leaves', item => {item.burnTime = 100})
+	event.modify('minecraft:spruce_leaves', item => {item.burnTime = 100})
+
+})
