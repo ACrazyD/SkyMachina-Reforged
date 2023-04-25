465c555daba5c7b4be10aef780bc5c6182af641b
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2023-04-25 16:17:33 -0700

    Quest Updates

--- kubejs/server_scripts/create/mixing.js
+++ kubejs/server_scripts/create/mixing.js
@@ -4,4 +4,5 @@ ServerEvents.recipes(e =>{
     e.recipes.create.mixing('minecraft:dirt', ['#minecraft:saplings', '#minecraft:saplings', '#minecraft:saplings', '#minecraft:saplings', '#minecraft:saplings', '#minecraft:saplings', 'minecraft:bone_meal', 'minecraft:bone_meal'])
     e.recipes.create.mixing([Fluid.of('water',50), 'dead_bush'] , ['#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves', '#minecraft:leaves'])
     e.recipes.create.mixing([Fluid.of('skymachina:mineral_water',100), 'skymachina:silt'], [Fluid.of('minecraft:water', 50), 'skymachina:iron_chunk', 'skymachina:iron_chunk', 'skymachina:copper_chunk', 'skymachina:copper_chunk'])
+    e.recipes.create.mixing('skymachina:pulp', [Fluid.of('minecraft:water', 50), 'thermal:sawdust', 'thermal:sawdust', 'thermal:sawdust', 'thermal:sawdust'])
 })
---
+++ kubejs/server_scripts/create/sawing.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    //SawDust
+    e.recipes.create.cutting('stick', 'thermal:sawdust')
+  })
